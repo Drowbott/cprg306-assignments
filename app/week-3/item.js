7b6822972e6e1ee5bbb80f0967b241133896651d
Drowbott <138698562+Drowbott@users.noreply.github.com>
2024-09-28 14:20:42 -0600

    Formatting updates for Assignment 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,15 +1,8 @@
-
-
-export default function Item ({name, quantity, category}){
-
-    return(
-      <div>
-        <li className="inline-block border border-black bg-blue-500 mt-5 mb-10 mx-6 px-5 py-3">
-          <span className="font-extrabold">{name}<br/></span>
-          <span className="text-black">{quantity}<br/></span>
-          <span className="text-black">{category}<br/></span>
-        </li>
-      </div>
-    );
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="w-96 ml-4 bg-blue-500 mt-4 mx-6 px-3 py-1">
+      <span className="font-bold text-2xl">{name}<br /></span>
+      <span>Buy {quantity} in {category}</span>
+    </li>
+  );
 };
-
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ItemList from './item-list';
+
 const Page = () => {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
-      <ItemList className="inline-block"/>
+    <main className=" p-2 ">
+      <h1 className="text-4xl font-bold mb-2">Shopping List</h1>
+      <ItemList />
     </main>
   );
 };
